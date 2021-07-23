eb72efee737bc3fcf66cc39c8942afbd9066d747
Alina Navarko <anavarko@aras.com>
2021-07-23 13:21:49 +0300

    Task1: Complemented Task1 of HW11

--- HomeWorks/hw_11/HW11_01/Program.cs
+++ HomeWorks/hw_11/HW11_01/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.Text.Json;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace HW11_01
 {
@@ -27,21 +28,23 @@ namespace HW11_01
             string songAuthor = "Mozart";
             int year = 1800;
             Genre genre = Interactions.GenreInput("1");
-
+            
             Song song = new(name, lenght, songAuthor, year, genre);
             var track = Interactions.GetSongData(song);
 
             Stopwatch newtonsoft = new();
             newtonsoft.Start();
             string newtonsoftResult = Newtonsoft.Json.JsonConvert.SerializeObject(track);
-            Console.WriteLine(newtonsoftResult);
             newtonsoft.Stop();
+            Console.WriteLine(newtonsoftResult);
+            string jsonFile = "jsonFileSong.json"; 
+            File.WriteAllText(jsonFile, newtonsoftResult);
 
             Stopwatch systemText = new();
             systemText.Start();
             string systemTextResult = System.Text.Json.JsonSerializer.Serialize(track);
-            Console.WriteLine(systemTextResult);
             systemText.Stop();
+            Console.WriteLine(systemTextResult);
 
             Console.WriteLine("Newtonsoft.Json: {0} ms", newtonsoft.ElapsedMilliseconds);
             Console.WriteLine("System.Text.Json: {0} ms", systemText.ElapsedMilliseconds);
