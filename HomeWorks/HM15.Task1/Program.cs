6dc7f2da6ea4e1c90f476cfc79bdd7a616b7f873
KuntsevichKiryl <kun-kir@yandex.ru>
2021-08-06 02:00:14 +0300

    HM15.Task1: add task realization

---
+++ HomeWorks/HM15.Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using HM15.Task1.Actions;
+using HM15.Task1.Validations;
+using System;
+using System.Reflection;
+
+namespace HM15.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PurchaseMotorcycle purchase = new PurchaseMotorcycle("4154 2699-62992373");
+            Type purchaseInfo = purchase.GetType();
+            foreach (PropertyInfo property in purchaseInfo.GetProperties())
+            {
+                foreach (Attribute attribute in property.GetCustomAttributes())
+                {
+                    if (attribute is not CustomCreditCardAttribute creditCardAttribute)
+                        continue;
+                    if (creditCardAttribute.IsValid(property.GetValue(purchase) as string))
+                        Console.WriteLine("Validation succsessful");
+                    else
+                        Console.WriteLine("Invalid credit card number");
+                }
+            }
+        }
+    }
+}
